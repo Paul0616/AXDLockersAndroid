619a92683819ea4cd5ea67f263afc1f6a83b6cb5
Paul Oprea <omd.paul@gmail.com>
2019-03-18 11:55:32 +0200

    first commit

--- AndroidClient/app/src/main/java/com/dotcode/duoline/axdlockers/MainActivity.java
+++ AndroidClient/app/src/main/java/com/dotcode/duoline/axdlockers/MainActivity.java
@@ -2,15 +2,78 @@ package com.dotcode.duoline.axdlockers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import info.androidhive.barcode.BarcodeReader;
+
+public class MainActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private BarcodeReader barcodeReader;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText("XXX");
+        // getting barcode instance
+        textView = (TextView) findViewById(R.id.textView);
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_fragment);
+    }
+
+    @Override
+    public void onScanned(final Barcode barcode) {
+        Log.e(TAG, "onScanned: " + barcode.displayValue);
+        barcodeReader.playBeep();
+
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Toast.makeText(getApplicationContext(), "Barcode: " + barcode.displayValue, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        textView.setText(barcode.displayValue);
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+        Log.e(TAG, "onScannedMultiple: " + barcodes.size());
+
+        String codes = "";
+        for (Barcode barcode : barcodes) {
+            codes += barcode.displayValue + ", ";
+        }
+
+        final String finalCodes = codes;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Barcodes: " + finalCodes, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        Toast.makeText(getApplicationContext(), "Camera permission denied!", Toast.LENGTH_LONG).show();
+        finish();
     }
 }
