c7ef792523a7f92a056f7ef55fd87e485cb30250
paul <omd.paul@gmail.com>
2019-04-03 19:33:50 +0300

    splash screen

--- AndroidClient/app/src/main/java/com/dotcode/duoline/axdlockers/MainActivity.java
+++ AndroidClient/app/src/main/java/com/dotcode/duoline/axdlockers/MainActivity.java
@@ -1,9 +1,13 @@
 package com.dotcode.duoline.axdlockers;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,35 +24,73 @@ public class MainActivity extends AppCompatActivity implements BarcodeReader.Bar
 
     private BarcodeReader barcodeReader;
     private TextView textView;
+    private boolean codeWasDetected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try
-        {
-            this.getSupportActionBar().hide();
-        }
-        catch (NullPointerException e){}
+//        try
+//        {
+//            this.getSupportActionBar().hide();
+//        }
+//        catch (NullPointerException e){}
         setContentView(R.layout.activity_main);
 
         // getting barcode instance
         textView = (TextView) findViewById(R.id.textView);
         barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_fragment);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              //  showAlert(MainActivity.this, "Test", "Mesaj de test");
+            }
+        });
     }
 
 
+    private void showAlert(Context ctx, String title, String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+        builder.setTitle(title);
+        builder.setMessage(msg);
+//        builder.setIcon(R.drawable.ic_error_outline_yellow_24dp);
+        builder.setCancelable(false);
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+            }
+        });
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                codeWasDetected = false;
+                barcodeReader.resumeScanning();
+
+            }
+        });
+        builder.show();
+    }
+
     @Override
     public void onScanned(final Barcode barcode) {
-        Log.e(TAG, "onScanned: " + barcode.displayValue);
-        barcodeReader.playBeep();
-///gschgdcghca cahg cdaauvd
-//        runOnUiThread(new Runnable() {
-//            @Override
-//            public void run() {
-//                Toast.makeText(getApplicationContext(), "Barcode: " + barcode.displayValue, Toast.LENGTH_SHORT).show();
-//            }
-//        });
-        textView.setText(barcode.displayValue);
+
+        if(!codeWasDetected) {
+            barcodeReader.pauseScanning();
+            final String qrCode = barcode.displayValue;
+            Log.e(TAG, "onScanned: " + qrCode);
+            barcodeReader.playBeep();
+            codeWasDetected = true;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    showAlert(MainActivity.this, "Test", qrCode);
+                }
+            });
+           // textView.setText(qrCode);
+        }
+
     }
 
     @Override
